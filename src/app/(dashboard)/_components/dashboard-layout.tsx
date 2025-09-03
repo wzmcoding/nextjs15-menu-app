fe7e6e8f58311a9368ab86f22936afa9f477898e
wzm <2791063637@qq.com>
2025-09-03 13:16:32 +0800

    feat(dashboard): 实现仪表板布局并添加侧边栏功能- 创建 DashboardLayout 组件，包含侧边栏和内容区域 - 在 Layout 组件中使用 DashboardLayout 组件替代原有布局 - 添加 Collapsible 组件实现侧边栏的展开和收起功能 - 更新 package.json，添加 radix-ui依赖

--- src/app/(dashboard)/_components/dashboard-layout.tsx
+++ src/app/(dashboard)/_components/dashboard-layout.tsx
@@ -1,13 +1,30 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
+import { Collapsible } from "radix-ui";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 type DashboardLayoutProps = {
   children: ReactNode;
 };
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <div>
-      DashboardLayout
+    <div className="flex">
+      <Collapsible.Root open={open} onOpenChange={setOpen}>
+        <Collapsible.Trigger asChild>
+          <Button size="icon" variant="outline">
+            <Menu />
+          </Button>
+        </Collapsible.Trigger>
+      </Collapsible.Root>
+
+      <Collapsible.Root open={open} onOpenChange={setOpen}>
+        <Collapsible.Content>content</Collapsible.Content>
+      </Collapsible.Root>
       {children}
     </div>
   );
